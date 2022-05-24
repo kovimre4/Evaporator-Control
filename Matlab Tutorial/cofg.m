5c23c609d75edadfa22ca1e49f313c34b248c95f
kovimre4 <kovimre4@gmail.com>
2022-05-24 09:09:16 +0100

    Linearising script

--- Matlab Tutorial/cofg.m
+++
@@ -1,9 +0,0 @@
-function out= cofg(weights, locations)
-% Comments are introduced by percent signs, as here.
-% These initial comments will be written to the terminal
-% if you type ’help cofg’. It is useful to remind the
-% user how to call the function by putting in the line:
-% cg = cofg(weights, locations)
-cg = 5
-out = weights * locations' / sum(weights);
-
