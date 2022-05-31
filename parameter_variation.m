c3aec5e541f9beb996fe2524bb91db0022a1f2ea
kovimre4 <kovimre4@gmail.com>
2022-05-31 10:49:17 +0100

    ...

--- parameter_variation.m
+++ parameter_variation.m
@@ -1,8 +1,15 @@
 clear
-
 % Initialise parameters
 initialise_parameters
 
+% Percentage tolerance for considering signal as settled
+tol = 0.01;
+
+% Load model
+model = 'plant_master';
+load_system(model);
+
+% 2) Set up the sweep parameters
 n_rhoAs = 20;
 n_Ms = 20;
 n_UA2s = 20;
@@ -11,33 +18,49 @@ rhoAs = linspace(10, 40, n_rhoAs);
 Ms = linspace(15, 25, n_Ms);
 UA2s = linspace(5, 8, n_UA2s);
 
-settling_times = zeros(n_rhoAs, n_Ms, n_UA2s, 3);
-warning('off','all')
+[RHOAS, MS, UA2S] = meshgrid(rhoAs, Ms, UA2s);
 
-for i_rhoA = 1:n_rhoAs
-    for i_M = 1:n_Ms
-        for i_UA2 = 1:n_UA2s
-            % Simulate
-            in = Simulink.SimulationInput('plant_master');
-            in = in.setVariable('rhoA', rhoAs(i_rhoA));
-            in = in.setVariable('M', Ms(i_M));
-            in = in.setVariable('UA2', UA2s(i_UA2));
-            out = sim(in);
+RHOAS = reshape(RHOAS, 1, []);
+MS = reshape(MS, 1, []);
+UA2S = reshape(UA2S, 1, []);
 
-            % Extract signals
-            t = out.tout;
-            L2 = out.yout(:, 1);
-            P2 = out.yout(:, 2);
-            X2 = out.yout(:, 3);
+n_sims = n_rhoAs*n_Ms*n_UA2s;
 
-            tol = 0.01;     % 1% within nominal value is considered settled
-            st_L2 = settling_time(t, L2, 1.0, tol);
-            st_P2 = settling_time(t, P2, 50.5, tol);
-            st_X2 = settling_time(t, X2, 25.0, tol);
-            
-            settling_times(i_rhoA, i_M, i_UA2, :) = [st_L2, st_P2, st_X2];
-        end
-    end
+% Create an array of SimulationInput objects and specify the sweep values for each simulation
+simIn(1:n_sims) = Simulink.SimulationInput(model);
+for i = 1:n_sims
+    simIn(i) = simIn(i).setVariable('rhoA', RHOAS(i));
+    simIn(i) = simIn(i).setVariable('M', MS(i));
+    simIn(i) = simIn(i).setVariable('UA2', UA2S(i));
 end
 
-save('settling_times.mat', 'settling_times', 'rhoAs', 'Ms', 'UA2s')
+% Simulate the model 
+simOut = parsim(simIn, 'UseFastRestart', 'on');
+
+% Process results (sts = settling times)
+sts_L2 = ones(size(simOut));
+sts_P2 = ones(size(simOut));
+sts_X2 = ones(size(simOut));
+
+for i= 1:n_sims
+    % Extract signals    
+    t = simOut(i).tout;
+    L2 = simOut(i).yout(:, 1);
+    P2 = simOut(i).yout(:, 2);
+    X2 = simOut(i).yout(:, 3);
+    
+    sts_L2(i) = settling_time(t, L2, 1.0, tol);
+    sts_P2(i) = settling_time(t, P2, 50.5, tol);
+    sts_X2(i) = settling_time(t, X2, 25.0, tol);
+end
+
+sts_L2 = reshape(sts_L2, n_rhoAs, n_Ms, n_UA2s);
+sts_P2 = reshape(sts_P2, n_rhoAs, n_Ms, n_UA2s);
+sts_X2 = reshape(sts_X2, n_rhoAs, n_Ms, n_UA2s);
+simOut = reshape(simOut, n_rhoAs, n_Ms, n_UA2s);
+
+settling_times(:,:,:,1) = sts_L2;
+settling_times(:,:,:,2) = sts_P2;
+settling_times(:,:,:,3) = sts_X2;
+
+save('settling_times_parallel.mat', 'settling_times', 'simOut', 'rhoAs', 'Ms', 'UA2s')
